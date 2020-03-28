705b41cd04740f0218e10f8eaf73c0e27d46a3db
Devin Ereshan <devinereshan@gmail.com>
2020-03-28 14:53:41 -0400

    Initial project structure

---
+++ src/drumSounds.js
@@ -0,0 +1,80 @@
+import { MembraneSynth, NoiseSynth, MetalSynth } from 'tone';
+
+const soundDefaults = {
+    kick: {
+        note: 'C1',
+        velocity: 1,
+        defaults: {
+            octaves : 5 ,
+            oscillator : {
+                type : 'sine'
+            } ,
+            envelope : {
+                attack : 0.001 ,
+                decay : 0.4 ,
+                sustain : 0.01 ,
+                release : 1.4 ,
+            }
+        },
+    },
+
+    snare: {
+        velocity: 1,
+        defaults: {
+            noise : {
+                type : 'pink'
+            } ,
+            envelope : {
+                attack : 0.005 ,
+                decay : 0.1 ,
+                sustain : 0
+            }
+        },
+    },
+
+    hihat: {
+        velocity: 0.7,
+        defaults: {
+            frequency : 1500 ,
+            envelope : {
+                attack : 0.001 ,
+                decay : 0.7 ,
+                release : 0.5
+            } ,
+            harmonicity : 3 ,
+            modulationIndex : 60 ,
+            resonance : 6000 ,
+            octaves : 1
+        },
+
+    }
+}
+
+export class Sound {
+    constructor(type) {
+        this.type = type;
+
+        this.duration = '8n';
+        this.time = '+0.001';
+
+        this.velocity = soundDefaults[type].velocity;
+        this.defaults = soundDefaults[type].defaults;
+
+        if (type === 'kick') {
+            this.note = soundDefaults.kick.note;
+            this.synth = new MembraneSynth(this.defaults).toMaster();
+        } else if (type === 'snare') {
+            this.synth = new NoiseSynth(this.defaults).toMaster();
+        } else if (type === 'hihat') {
+            this.synth = new MetalSynth(this.defaults).toMaster();
+        }
+    }
+
+    play() {
+        if (this.type === 'kick') {
+            this.synth.triggerAttackRelease(this.note, this.duration, this.time, this.velocity);
+        } else {
+            this.synth.triggerAttackRelease(this.duration, this.time, this.velocity);
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,18 +1,57 @@
-import { Synth } from 'tone';
+import * as Tone from 'tone';
+import { Sound } from './drumSounds';
+import * as Nexus from './lib/NexusUI';
 
-const synth = new Synth().toMaster();
 
-function component() {
-    const element = document.createElement('button');
+window.addEventListener('load', () => {
 
-    element.innerHTML = "Play Note";
+    const sequencer = new Nexus.Sequencer('#step-sequencer', {
+        'size': [768, 144],
+        'rows': 3,
+        'columns' : 16
+    });
+    sequencer.colorize('accent', "#f0f");
+    sequencer.colorize('fill', "#000");
+    sequencer.colorize('mediumLight', "#0ff");
 
-    return element;
-}
+    console.log(sequencer.matrix);
 
-const playButton = component();
-playButton.addEventListener('click', () => {
-    synth.triggerAttackRelease('C3', '4n');
+    const kick = new Sound('kick');
+    const snare = new Sound('snare');
+    const hihat = new Sound('hihat');
+
+
+    const synths = {
+        'kick' : kick,
+        'snare' : snare,
+        'hihat' : hihat
+    }
+    const synthNames = ['kick', 'snare', 'hihat'];
+
+    const playButton = document.querySelector('.play-button');
+    const pauseButton = document.querySelector('.pause-button');
+
+    playButton.addEventListener('click', () => {
+        Tone.Transport.start();
+    });
+
+    pauseButton.addEventListener('click', () => {
+        Tone.Transport.stop();
+    });
+
+    const loop = new Tone.Sequence(function(time, col) {
+        const columnStates = sequencer.matrix.column(col);
+
+        columnStates.forEach(function(isArmed, index){
+            if(isArmed) {
+                const synth = synthNames[index];
+                synths[synth].play();
+            }
+        });
+
+        Tone.Draw.schedule(function(){
+            sequencer.next();
+        }, time);
+    }, [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], "16n").start(0);
 });
 
-document.body.appendChild(playButton);
